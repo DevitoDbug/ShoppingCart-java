36e9cd169879d4288baa6d4cf0342dd3557cfba1
DevitoDbug <Davidochiengy@gmail.com>
2023-10-03 20:04:44 +0300

    Clean code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,4 @@
 public class Main {
 public static void main(String[] args){
-    Item item1 = new Item("clothes", 789.5 , 1);
-    Item item2 = new Item("shoes", 60 , 1);
-    Item item3 = new Item("trousers", 300 , 1);
-
-    ShoppingCart cart = new ShoppingCart();
-    cart.AddItemToCart(item1);
-    cart.AddItemToCart(item2);
-    cart.AddItemToCart(item3);
-
-    System.out.println(cart.GetAllItems());
-    cart.ForgetItem(1);
-
-    System.out.println(cart.GetAllItems());
 }
 }
