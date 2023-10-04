79ec1b98a1a9e4e8d0338b8102981f20c2283dad
DevitoDbug <Davidochiengy@gmail.com>
2023-10-04 21:16:25 +0300

    Create unit tests using jUnit

---
+++ test/ShoppingCartTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @Test
+    void getAllItems() {
+    }
+
+    @Test
+    void addItemToCart() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void restoreItem() {
+    }
+
+    @Test
+    void forgetItem() {
+    }
+
+    @Test
+    void totalPrice() {
+    }
+
+    @Test
+    void numberOfItems() {
+    }
+}
